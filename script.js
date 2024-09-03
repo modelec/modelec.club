2df22ab05110b714adf1d36089475e74d809aa1e
Félix MARQUET <fpmovie4@gmail.com>
2024-09-03 09:52:04 +0200

    Fix carousel delay and link display

--- script.js
+++ script.js
@@ -18,5 +18,5 @@ function showSlides(n) {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "flex";
-    setTimeout(showSlides, 2000);
+    setTimeout(showSlides, 5000);
 }
