e70e1af157e68b5f3a0bf513af9f92983e533451
Félix MARQUET <fpmovie4@gmail.com>
2024-09-03 09:47:47 +0200

    Add carousel for partenaires

---
+++ script.js
@@ -0,0 +1,22 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "flex";
+    setTimeout(showSlides, 2000);
+}
