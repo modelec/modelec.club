bfb63e923f40cb3684728533a0eb278a8e81785f
TitiLaPierre <tit.branger@gmail.com>
2024-10-09 11:29:36 +0200

    Added icons from Iconoir

--- src/components/socialnetwork/socialnetwork.tsx
+++ src/components/socialnetwork/socialnetwork.tsx
@@ -1,41 +1,16 @@
 import React from "react";
 
 interface SocialNetworkProps {
-    icon: string;
+    Icon: React.FC;
     link: string;
     name: string;
 }
 
-interface SocialNetworkSmallProps {
-    icon: string;
-    link: string;
-}
-
-interface SocialNetworksProps {
-    networks: SocialNetworkProps[];
-}
-
-const SocialNetwork: React.FC<SocialNetworkProps> = ({ icon, link, name }) => {
+export const SocialNetwork: React.FC<SocialNetworkProps> = ({ Icon, link, name }) => {
     return (
-        <a href={link} className={"social-network"}>
-            <img src={icon} alt={name}/>
-            <p className={"social-network-name"}>{name}</p>
+        <a href={link} className={"media-link"} target="_blank">
+            <Icon />
+            {name}
         </a>
-    );
-}
-
-export const SocialNetworkSmall: React.FC<SocialNetworkSmallProps> = ({ icon, link }) => {
-    return (
-        <a href={link} className={"social-network-small"}>
-            <img src={icon} alt={link} className={"social-network-small-icon"}/>
-        </a>
-    );
-}
-
-export const SocialNetworks: React.FC<SocialNetworksProps> = ({ networks }) => {
-    return (
-        <div className={"social-networks"}>
-            {networks.map((network, index) => <SocialNetwork key={index} {...network} />)}
-        </div>
     );
 }
