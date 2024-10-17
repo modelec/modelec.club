7115a383161a9933eb6cc4bdff9a8e06a0085506
TitiLaPierre <tit.branger@gmail.com>
2024-10-17 15:56:54 +0200

    Updated Projects page

---
+++ src/components/timeline/timeline.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+import './timeline.css';
+
+interface StatCardProps {
+  type: "stat";
+  data: string;
+  label: string;
+};
+
+interface ImageCardProps {
+  type: "image";
+  src: string;
+  alt: string;
+  fit: "contain" | "cover";
+};
+
+interface TimelineProjectProps {
+  title: string;
+  date: string;
+  banner?: string;
+  paragraphs: React.ReactNode[];
+  cards?: (StatCardProps|ImageCardProps)[];
+};
+
+interface TimelineProps {
+  projects: TimelineProjectProps[];
+};
+
+const TimelineProjectCard: React.FC<StatCardProps | ImageCardProps> = (props) => {
+  if (props.type === "stat") {
+    const { data, label } = props as StatCardProps;
+    return (
+      <div className={'timeline-project-card card_stat'}>
+        <p className={'timeline-project-card-number'}>{data}</p>
+        <p className={'timeline-project-card-subtext'}>{label}</p>
+      </div>
+    );
+  } else if (props.type === "image") {
+    const { src, alt, fit } = props as ImageCardProps;
+    return (
+      <div className={`timeline-project-card card_image_${fit}`}>
+        <img className={"timeline-project-card-image"} src={src} alt={alt} />
+      </div>
+    );
+  }
+};
+
+export const TimelineProject: React.FC<TimelineProjectProps> = ({ title, banner, date, paragraphs, cards }) => {
+  return (
+    <div className={'timeline-project'}>
+      <div className={'timeline-project-content'}>
+        <h3 className={'timeline-project-title'}>{title}</h3>
+        <p className={'timeline-project-date'}>{date}</p>
+      </div>
+      { banner && <img className={'timeline-project-banner'} src={banner} alt={title} /> }
+      <div className={'timeline-project-description'}>
+        { paragraphs.map((paragraph, i) => <p className={'timeline-project-paragraph'} key={i}>{paragraph}</p>) }
+      </div>
+      <div className={'timeline-project-cards'}>
+        { cards && cards.map((card, i) => <TimelineProjectCard key={i} {...card} />) }
+      </div>
+    </div>
+  );
+}
+
+export const Timeline: React.FC<TimelineProps> = ({ projects }) => {
+  return (
+    <div className={'timeline'}>
+      <h4 className={'timeline-tag'}>Aujourd'hui</h4>
+      {
+        projects.map((project, i) => {
+          return (
+            <TimelineProject key={i} {...project} />
+          );
+        })
+      }
+      <h4 className={'timeline-tag'}>Il y a quelques temps</h4>
+    </div>
+  );
+}
--- src/pages/projets/Projets.tsx
+++ src/pages/projets/Projets.tsx
@@ -1,51 +1,72 @@
 import React from 'react';
-import { ProjetsHeader } from '../../components/projetheader/projetheader';
-import {
-  ProjectBigCard,
-  ProjectSmallCard,
-} from '../../components/projectcard/projectcard';
-
+import { Timeline, TimelineProject } from '../../components/timeline/timeline';
 import './Projets.css';
 
 const Projets: React.FC = () => {
   return (
     <>
-      <ProjetsHeader />
-      <div className={'project-header-container page-contrast'}>
-        <ProjectBigCard
-          imgtitle={'https://r2.modelec.club/cdf.png'}
-          lefttext={{
-            __html:
-              "1 robot<br>3000 heures<br>0 compétances<br>10 étudiants<br>1ère participation<br><span style='color: #0693E3;'>14 ème</span> sur 82 équipes",
-          }}
-          rightimage={'https://r2.modelec.club/serge.png'}
-          downtitle={'Notre robot'}
-          downcontent={
-            "Voici Serge, notre robot pour la coupe de France de robotique 2024. Il est équipé de 2 roues motrices, 2 roues codeuses, 3 pinces et 2 bras d'orientation pour les panneaux solaires. Il est également équipé d'une caméra pour la vision et d'un lidar pour la détection d'adversaire et la triangulation. Coté hardware, il est géré par un raspberry pi 5 et un arduino mega. Côté software, il est géré par des programmes C++ fait sur mesure dont le code est disponible sur notre github. A coté Serge est accompagné de 2 Pixels, ses PAMI (Partie Autonome Mobile Intelligente) qui sont des robots autonomes qui peuvent se déplacer et se repérer dans l'espace. Ils sont gérés par des arduino uno."
+      <h1 className={'page-title'}>Cette année</h1>
+      <TimelineProject
+        title={'La Coupe de France de Robotique 2025'}
+        date={'À venir'}
+        paragraphs={[
+          <>Cette année encore, nous participons à la <u>Coupe de France de Robotique</u> pour notre <b>deuxième participation</b> !</>,
+        ]}
+      />
+      <h1 className={'page-title'}>Remonter dans le Temps</h1>
+      <Timeline
+        projects={[
+          {
+            title: 'Serge : Notre robot',
+            date: '2024',
+            banner: 'https://r2.modelec.club/serge.png',
+            paragraphs: [
+              <>Voici Serge, notre robot pour la <u>coupe de France de Robotique</u> 2024 !</>,
+              <>Il est équipé de <b>2 roues motrices</b>, <b>2 roues codeuses</b>, <b>3 pinces</b> et <b>2 bras d'orientation</b> pour les panneaux solaires. Il est également équipé d'<b>une caméra</b> pour la vision et d'<b>un LiDAR</b> pour la détection d'adversaire et la triangulation.</>,
+              <>Coté <u>Hardware</u>, il est géré par un <b>Raspberry Pi 5</b> et un <b>Arduino Mega</b>.</>,
+              <>Côté <u>Software</u>, il est géré par des programmes <b>C++</b> faits sur mesure dont le code est disponible sur notre Github.</>,
+              <>Serge n'est pas seul, il est accompagné de <u>2 Pixels</u>, ses <b>Partie Autonome Mobile Intelligente</b> ou <u>PAMI</u> qui sont des robots autonomes gérés par des <b>Arduino Uno</b> qui peuvent se déplacer et se repérer dans l'espace.</>
+            ],
+            cards: [
+              { type: 'stat', data: '3000', label: 'Heures de travail' },
+              { type: 'stat', data: '10', label: 'Étudiants' },
+              { type: 'stat', data: '2024', label: 'Première participation' },
+              { type: 'stat', data: '14', label: '/82 équipes' },
+              { type: 'image', src: 'https://r2.modelec.club/cdf.png', alt: 'Logo Coupe de France de Robotique', fit: 'contain' },
+            ],
+          },
+          {
+            title: 'Construction de la table de jeu',
+            date: '2024',
+            paragraphs: [
+              <>Afin de pouvoir tester notre robot, nous avons construit une <b>table de jeu</b> pour la <u>coupe de France de Robotique</u>.</>,
+              <>Elle est faite en <b>bois</b> et en <b>vinyle</b>.</>,
+            ],
+            cards: [
+              { type: 'image', src: 'https://r2.modelec.club/table2024.jpg', alt: 'Table de jeu pour la Coupe de France de Robotique', fit: 'cover' },
+            ],
+          },
+          {
+            title: 'PAMI v1',
+            date: '2024',
+            paragraphs: [
+              <>Les <b>PAMI v1</b> sont des <b>robots autonomes</b> qui peuvent se déplacer et se repérer dans l'espace.</>,
+              <>Ils sont gérés par des <b>Arduino Uno</b>.</>,
+            ],
+            cards: [
+              { type: 'image', src: 'https://r2.modelec.club/PAMI-V1.png', alt: 'PAMI v1', fit: 'cover' },
+            ],
+          },
+          {
+            title: 'Digitalisation du bureau',
+            date: '2024',
+            paragraphs: [
+              <>Afin de faciliter la gestion du club, nous avons décidé de <b>digitaliser</b> le <b>bureau</b>.</>,
+            ],
           }
-        />
-        <ProjectSmallCard
-          img={'https://r2.modelec.club/table2024.jpg'}
-          title={'Constructions tables de jeu CDF'}
-          content={
-            'Afin de pouvoir tester notre robot, nous avons construit 1 table de jeu pour la coupe de France de robotique. Elle est faites en bois et en vynile.'
-          }
-        />
-        <ProjectSmallCard
-          img={'https://r2.modelec.club/PAMI-V1.png'}
-          title={'PAMI V1'}
-          content={
-            "Les PAMI V1 sont des robots autonomes qui peuvent se déplacer et se repérer dans l'espace. Ils sont gérés par des arduino uno."
-          }
-        />
-        <ProjectSmallCard
-          img={'https://r2.modelec.club/digitalisation.png'}
-          title={'Digitalisation du bureau'}
-          content={
-            'Afin de faciliter la gestion du club, nous avons décidé de digitaliser le bureau.'
-          }
-        />
-      </div>
+        ]}
+      />
+
     </>
   );
 };
