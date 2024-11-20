b9f9da8500a0e016cc076d0ecae79aceab6940c4
Félix MARQUET <fpmovie4@gmail.com>
2024-11-20 19:59:47 +0100

    C pu pété V3

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false,
   },
 
   component: {
+    specPattern: "cypress/components/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/component.ts",
     devServer: {
       framework: "react",
       bundler: "vite",
