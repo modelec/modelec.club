4b830dfc0ccd6fdc78cc99808db7313db4eae35c
Félix MARQUET <fpmovie4@gmail.com>
2024-11-20 19:49:11 +0100

    C pu pété V2

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false,
   },
 
