43eba5cd56292f32c10009795a6734ffc4f357e7
Félix MARQUET <fpmovie4@gmail.com>
2024-11-20 20:03:42 +0100

    C pu pété V4

--- cypress/e2e/homeRedirections.cy.ts
+++ cypress/e2e/homeRedirections.cy.ts
@@ -4,17 +4,17 @@ describe('Home Page Redirections', () => {
   });
 
   it('should redirect to Projets page', () => {
-    cy.get('a[href="/projets/"]').click();
+    cy.get('a[href="/projets/"]').first().click();
     cy.url().should('include', '/projets/');
   });
 
   it('should redirect to Partenaires page', () => {
-    cy.get('a[href="/partenaires/"]').click();
+    cy.get('a[href="/partenaires/"]').first().click();
     cy.url().should('include', '/partenaires/');
   });
 
   it('should redirect to Contact page', () => {
-    cy.get('a[href="/contact/"]').click();
+    cy.get('a[href="/contact/"]').first().click();
     cy.url().should('include', '/contact/');
   });
 });
